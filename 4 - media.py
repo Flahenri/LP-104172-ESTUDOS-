d75dd6c5d961b2771b7b255236e6f2f4c8b9cedb
Flavio Henrique ( Aluno ) - <142919997+Flahenri@users.noreply.github.com>
2026-08-21 16:28:29 -0300

    Add files via upload

---
+++ 4 - media.py
@@ -0,0 +1,18 @@
+import os
+
+# ligar terminal
+os.system('cls')
+
+nome = input('Digite seu nome: ')
+idade = int(input('Digite Sua idade:'))
+primeira_nota = float(input('Digite a primeira nota: '))
+segunda_nota = float(input('Digite a Segunda Nota: '))
+
+media = (primeira_nota + segunda_nota)
+
+print('\n= EXIBINDO DADOS =')
+print('nome: ', nome)
+print('idade: ', idade)
+print('primeira nota: ', primeira_nota)
+print('segunda nota: ', segunda_nota)
+print('Média: ', media)
---
+++ 5 - desconto.py
@@ -0,0 +1,17 @@
+import os
+
+# limpar o terminal
+
+os.system('cls')
+
+print('SOLICITANDO DADOS =')
+valor = float(input('digite o valor: '))
+
+# CALCULANDO
+# DESCONTANDO 10%
+
+desconto = valor * 0.10
+valor_com_desconto = valor - desconto
+
+print('\n EXIBINDO DADOS =')
+print('valor com desconto de 10%: ', valor_com_desconto)
